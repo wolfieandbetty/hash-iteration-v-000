8cc66a6aafbeba86a670c449cb6bbcfb349b3316
Cat Norris <cannenorris@yahoo.com>
2017-12-19 02:10:59 +0000

    automatically backed up by learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -8,9 +8,11 @@
 
 def select_winner(passengers)
 winner = " "
+
 passengers.each do |suite, name|
   if suite == :suite_a && name.start_with?("A")
-  winner = name
+    winner = name
+   end
 end
-end
+
 winner
